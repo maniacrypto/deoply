974a91cbbcf65a679fd33966cc7072053b1dc17d
maniacrypto <maniacrypto97@gmail.com>
2026-05-04 13:44:32 +0500

    config change

--- src/wagmiConfig.tsx
+++ src/wagmiConfig.tsx
@@ -1,20 +1,23 @@
 import { createConfig, http } from 'wagmi'
 import { base } from '@wagmi/core/chains'
 import { injected, walletConnect } from 'wagmi/connectors'
+import { getDefaultConfig } from '@rainbow-me/rainbowkit'
 
-export const config = createConfig({
+export const config = getDefaultConfig ({
+      appName: 'My Web3 App',
+  projectId: import.meta.env.VITE_WALLETCONNECT_ID,
   chains: [base],
- connectors: [
-    injected(),
+//  connectors: [
+//     injected(),
 
-    walletConnect({
-      projectId: import.meta.env.VITE_WALLETCONNECT_ID,
-      showQrModal: true,
-    }),
-  ],
-  transports: {
-    [base.id]: http(),
-  },
+//     walletConnect({
+//       projectId: import.meta.env.VITE_WALLETCONNECT_ID,
+//       showQrModal: true,
+//     }),
+//   ],
+//   transports: {
+//     [base.id]: http(),
+//   },
 
  
 })
