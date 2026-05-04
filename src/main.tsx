9adc246eae56b17b5bf4e4240f6fa06db74923fa
maniacrypto <maniacrypto97@gmail.com>
2026-05-04 12:42:11 +0500

    changes of wagmi

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
+import '@rainbow-me/rainbowkit/styles.css';
+import {config} from "./wagmiConfig.tsx";
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+import { WagmiProvider } from 'wagmi';
+import { base, baseSepolia, mainnet, sepolia } from 'wagmi/chains';
+// Create Query Client
+const queryClient = new QueryClient();
 
+// const config = getDefaultConfig({
+//   appName: 'My Dapp',
+//   chains: [base],
+// });
 createRoot(document.getElementById('root')!).render(
- 
+     <QueryClientProvider client={queryClient}>
+    <WagmiProvider config={config}>
+      <RainbowKitProvider>
     <App />
+    </RainbowKitProvider>
+    </WagmiProvider>
+    </QueryClientProvider>
   
 );
---
+++ src/wagmiConfig.tsx
@@ -0,0 +1,12 @@
+import { createConfig, http } from '@wagmi/core'
+import { base, baseSepolia } from '@wagmi/core/chains'
+
+export const config = createConfig({
+
+  chains: [base,baseSepolia],
+   transports: {
+    [base.id]: http(),
+    [baseSepolia.id]:http()
+    
+  },
+});
